6a70b30b6fe7bdf21d73dac68cd2fcb11edb2842
Kiffin Gish <kiffin.gish@planet.nl>
2022-12-29 16:54:55 +0100

    Chapter 5.1: Updated blue_level.xml and scene file parser with textures.

--- src/my_game/util/scene_file_parser.ts
+++ src/my_game/util/scene_file_parser.ts
@@ -1,30 +1,37 @@
 /*
  * File: scene_file_parser.ts
  *
- * Scene file parsing utils
+ * Scene file parsing utils.
  */
 
 import { vec2 } from 'gl-matrix';
 
 // Engine utility stuff
 import engine from '../../engine/index';
-import Renderable from '../../engine/renderable';
+import Renderable from '../../engine/renderables/renderable';
 
 class SceneFileParser {
-    xml: XMLDocument;
+    mSceneXml: XMLDocument;
 
-    constructor(xml: XMLDocument) {
-        this.xml = xml;
+    constructor(sceneFilePath: string) {
+        this.mSceneXml = engine.text.get(sceneFilePath) as XMLDocument;
+    }
+
+    _getElm(tagElm: string) {
+        const theElm = this.mSceneXml.getElementsByTagName(tagElm);
+        if (theElm.length === 0) {
+            console.error('Warning: Level element:[' + tagElm + ']: is not found!');
+        }
+        return theElm;
     }
 
     parseCamera() {
-        const camElm = getElm(this.xml, 'Camera');
+        const camElm = this._getElm('Camera');
         const cx = Number(camElm[0].getAttribute('CenterX'));
         const cy = Number(camElm[0].getAttribute('CenterY'));
         const w = Number(camElm[0].getAttribute('Width'));
         const viewport: string[] | number[] = camElm[0].getAttribute('Viewport')?.split(' ') || [ 0, 0, 0, 0 ];
         const bgColor: string[] | number[] = camElm[0].getAttribute('BgColor')?.split(' ') || [ 0, 0, 0, 0 ];
-
         // make sure viewport and color are number
         for (let j = 0; j < 4; j++) {
             bgColor[j] = Number(bgColor[j]);
@@ -41,7 +48,7 @@ class SceneFileParser {
     }
 
     parseSquares(sqSet: Renderable[]) {
-        const elm = getElm(this.xml, 'Square');
+        const elm = this._getElm('Square');
         let x, y, w, h, r, c, sq;
         for (let i = 0; i < elm.length; i++) {
             x = Number(elm.item(i)?.attributes.getNamedItem('PosX')?.value);
@@ -52,7 +59,7 @@ class SceneFileParser {
             c = elm.item(i)?.attributes.getNamedItem('Color')?.value.split(' ') || [ 0, 0, 0, 0 ];
             sq = new engine.Renderable();
             // make sure color array contains numbers
-            for (let j = 0; j < 4; j++) {
+            for (let j = 0; j < 3; j++) {
                 c[j] = Number(c[j]);
             }
             sq.setColor(c as number[]);
@@ -62,15 +69,30 @@ class SceneFileParser {
             sqSet.push(sq);
         }
     }
-}
 
-function getElm(xmlContent: XMLDocument, tagElm: string) {
-    const theElm = xmlContent.getElementsByTagName(tagElm);
-    if (theElm.length === 0) {
-        console.error('Warning: Level element:[' + tagElm + ']: is not found!');
+
+    parseTextureSquares(sqSet: Renderable[]) {
+        const elm = this._getElm('TextureSquare');
+        let x, y, w, h, r, c, t, sq;
+        for (let i = 0; i < elm.length; i++) {
+            x = Number(elm.item(i)?.attributes.getNamedItem('PosX')?.value);
+            y = Number(elm.item(i)?.attributes.getNamedItem('PosY')?.value);
+            w = Number(elm.item(i)?.attributes.getNamedItem('Width')?.value);
+            h = Number(elm.item(i)?.attributes.getNamedItem('Height')?.value);
+            r = Number(elm.item(i)?.attributes.getNamedItem('Rotation')?.value);
+            c = elm.item(i)?.attributes.getNamedItem('Color')?.value.split(' ') || [ 0, 0, 0, 0 ];
+            t = elm.item(i)?.attributes.getNamedItem('Texture')?.value || '';
+            sq = new engine.TextureRenderable(t);
+            // make sure color array contains numbers
+            for (let j = 0; j < 4; j++)
+                c[j] = Number(c[j]);
+            sq.setColor(c as number[]);
+            sq.getXform().setPosition(x, y);
+            sq.getXform().setRotationInDegree(r); // In Degree
+            sq.getXform().setSize(w, h);
+            sqSet.push(sq);
+        }
     }
-    return theElm;
 }
-
 
 export default SceneFileParser;
